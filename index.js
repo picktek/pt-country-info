563c5d898df2e7cc4ec4a5796e6c7784dbbe2566
luka dodelia <luka@picktek.com>
2018-11-28 17:51:25 +0400

    now seems to proxy aliases so this is work around

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const ip2countrify = require('ip2countrify');
 
-module.exports = async (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+module.exports = (req, res) => {
+  let reqIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  if (reqIP === '127.0.0.1') {
+    reqIP = req.headers['x-real-ip'];
+  }
+ 
   ip2countrify.lookup(
-    ip,
+    reqIP,
     (ip, results, error) => {
       if (error) {
         return res.end('An error has occurred: ' + error);
       }
 
-      results.ip = ip;
+      results.ip = reqIP;
 
       res.setHeader('Content-Type', 'application/json; charset=utf-8');
       res.end(JSON.stringify(results));
